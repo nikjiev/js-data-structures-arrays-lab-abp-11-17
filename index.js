1a6ebfd31fdbe0ba453225c53019b75c9d9d198c
Nik Jiev <n.arabadjiev@gmail.com>
2017-11-19 05:24:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function prependDriver(name) {
 }
 
 function removeLastDriver() {
-  const copyOfDrivers = drivers.slice(2);
+  const copyOfDrivers = drivers.slice(0, drivers.length - 1);
   return copyOfDrivers;
 }
 
